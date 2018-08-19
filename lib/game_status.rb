c8779639436bf065faafd46e10ba33ada1678dd1
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-08-19 13:25:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,16 +15,35 @@ def position_taken?(board, index)
     [6,4,2]
     # ETC, an array for each win combination
   ]
+<<<<<<< HEAD
+=======
+
+def position_taken?(board, index)
+  board[index] == " " || board[index] == ""
+  board[index] == "X" || board[index] == "O"
+end 
+>>>>>>> 47385bb09049ad1c0eb912aee6d2c2b9bbbc9adb
 
 def won?(board)
   WIN_COMBINATIONS.each do |combo| #iterate over WIN_COMBINATIONS
     position_1 = board[combo[0]]
     position_2 = board[combo[1]]
     position_3 = board[combo[2]]
+<<<<<<< HEAD
   if position_1 == position_2 && position_2 == position_3 && position_taken?(board, combo[0])
     return combo #it wants the indexes, not the board markers
   end
 	#do not set an else, as otherwise you are forcing a return before the loop finishes. 
   end
   false
+=======
+    #Now check if all of these have the same marker and use position_taken? method to see if there is a marker in one of them
+  if position_1 === position_2 && position_2 === position_3
+    position_taken?(board, position_3.to_i)
+  else 
+    false
+  end
+end
+  #if it makes it through the loop without finding a winning combo, then return false. 
+>>>>>>> 47385bb09049ad1c0eb912aee6d2c2b9bbbc9adb
 end
